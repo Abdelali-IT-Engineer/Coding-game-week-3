2c47198833ba5f2bb2ee8c71cf15dea3342057ee
Abdelali-IT-Engineer <165439618+Abdelali-IT-Engineer@users.noreply.github.com>
2024-10-21 09:34:05 +0100

    Update Electronics.java

--- Coding game week 3/Electronics.java
+++ Coding game week 3/Electronics.java
@@ -1,19 +1,16 @@
 public class Electronics extends Product {
     private String brand;
     private int warranty ;
-    public Electronics (String brand , int warranty ,String Name ,double price ,int quantity){
-        super(Name, price , quantity);
+    
+    public Electronics (String brand, int warranty, String Name, double price, int quantity){
+        super(Name, price, quantity);
         this.brand  = brand  ;
         this.warranty = warranty;
-        }
+    }
 
-        @Override
-        public void displayInfo(){
-            super.displayInfo();
-            System.out.println("Brand: " + brand + ", Warranty: " + warranty + "ans" );
-            }
-
-        
-        
-    
+    @Override
+    public void displayInfo(){
+        super.displayInfo();
+        System.out.println("Brand: " + brand + ", Warranty: " + warranty + "ans");
+    }
 }
